ad69e54170869caef7f4dde42f47a053ae64c1fb
KAMETAN0730 <kametan0730@gmail.com>
2016-10-28 00:37:29 +0900

    Update Copyright

--- src/pocketmine/plugin/FolderPluginLoader.php
+++ src/pocketmine/plugin/FolderPluginLoader.php
@@ -1,4 +1,20 @@
 <?php
+
+/*
+ * DevTools plugin for PocketMine-MP
+ * Copyright (C) 2014 PocketMine Team <https://github.com/PocketMine/DevTools>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+*/
+
 namespace pocketmine\plugin;
 
 use pocketmine\event\plugin\PluginDisableEvent;
