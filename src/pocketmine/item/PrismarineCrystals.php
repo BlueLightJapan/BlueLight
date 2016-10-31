0215ee3eb5b0968f3245b861edb06cfb067a7789
KAMETAN0730 <kametan0730@gmail.com>
2016-10-31 16:57:28 +0900

    Add 0.16 Items

---
+++ src/pocketmine/item/PrismarineCrystals.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ *
+ *  ____          
+ * |  __|_              _
+ * | |__| |      _    _(_)_ __   ___
+ * |  __| |_   _| |  | | | '_ \ / _ \
+ * | |__| | | | | |/\| | | | | | (_) |
+ * |____|_|\ \/ \__/\__/_|_| |_|\___ |
+ *         _|  /                 __| |
+ *        |___/                 |____/
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @author H4PM Team
+ * @link http://www.github.net/H4PM
+ * 
+ *
+*/
+
+namespace pocketmine\item;
+
+
+class PrismarineCrystals extends Item{
+	public function __construct($meta = 0, $count = 1){
+		parent::__construct(self::PRISMARINE_CRYSTALS, $meta, $count, "Prismarine Crystals");
+	}
+}
