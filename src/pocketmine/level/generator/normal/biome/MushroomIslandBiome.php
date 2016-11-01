e129b4b862cfbbc89f8b8e870b24d391091c0fa8
KAMETAN0730 <kametan0730@gmail.com>
2016-11-01 18:42:43 +0900

    Add Biome

---
+++ src/pocketmine/level/generator/normal/biome/MushroomIslandBiome.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+ *   ____  _            _      _       _     _
+ *  |  _ \| |          | |    (_)     | |   | |
+ *  | |_) | |_   _  ___| |     _  __ _| |__ | |_
+ *  |  _ <| | | | |/ _ \ |    | |/ _` | '_ \| __|
+ *  | |_) | | |_| |  __/ |____| | (_| | | | | |_
+ *  |____/|_|\__,_|\___|______|_|\__, |_| |_|\__|
+ *                                __/ |
+ *                               |___/
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @author BlueLightJapan Team
+ * 
+*/
+
+namespace pocketmine\level\generator\normal\biome;
+
+use pocketmine\block\Block;
+
+class MushroomIslandBiome extends NormalBiome{
+
+	public function __construct(){
+		$this->setGroundCover([
+			Block::get(110, 0),
+			Block::get(Block::DIRT, 0),
+			Block::get(Block::DIRT, 0),
+			Block::get(Block::DIRT, 0),
+			Block::get(Block::DIRT, 0),
+		]);
+
+		$this->setElevation(63, 97);
+	}
+
+	public function getName(){
+		return "Mushroom Island";
+	}
+
+
+}
