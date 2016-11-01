9ca10e844e701c1f041d510689a44e0b54939516
haniokasai♡ <htek@haniokasai.com>
2016-11-01 19:27:31 +0900

    Update ci.php

--- ci.php
+++ ci.php
@@ -1,12 +1,13 @@
 <?php
 // import from clearsky/ci-test
+$time = time();
 $server = proc_open(PHP_BINARY . " src/pocketmine/PocketMine.php --no-wizard --disable-readline", [
 	0 => ["pipe", "r"],
 	1 => ["pipe", "w"],
 	2 => ["pipe", "w"]
 ], $pipes);
 fwrite($pipes[0], "version\nmakeserver\nstop\n\n");
-while(!feof($pipes[1])){
+while(!feof($pipes[1]) and time()-$time<60*3){
 	echo fgets($pipes[1]);
 }
 fclose($pipes[0]);
