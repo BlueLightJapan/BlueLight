9af083cb003931efbdc1cb75d1d6d4453ee76d4d
KAMETAN0730 <kametan0730@gmail.com>
2016-11-12 15:58:35 +0900

    0.17 Add Network

---
+++ 0.17/protocol/ClientToServerHandshakePacket.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+ *   ____  _            _      _       _     _
+ *  |  _ \| |          | |    (_)     | |   | |
+ *  | |_) | |_   _  ___| |     _  __ _| |__ | |_
+ *  |  _ <| | | | |/ _ \ |    | |/ _` | '_ \| __|
+ *  | |_) | | |_| |  __/ |____| | (_| | | | | |_
+ *  |____/|_|\__,_|\___|______|_|\__, |_| |_|\__|
+ *                                __/ |
+ *                               |___/
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @author BlueLightJapan Team
+ * 
+*/
+
+namespace pocketmine\network\protocol;
+
+#include <rules/DataPacket.h>
+
+
+class ClientToServerHandshakePacket extends DataPacket{
+	const NETWORK_ID = Info::CLIENT_TO_SERVER_HANDSHAKE_PACKET;
+
+	public function decode(){
+	}
+
+	public function encode(){
+		$this->reset();
+	}
+
+}
