3d353e067000506c5acbc07ca5c6b39b68c8045b
KAMETAN0730 <kametan0730@gmail.com>
2016-11-19 19:25:49 +0900

    Update

---
+++ src/pocketmine/item/Door.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ *   ____  _            _      _       _     _
+ *  |  _ \| |          | |    (_)     | |   | |
+ *  | |_) | |_   _  ___| |     _  __ _| |__ | |_
+ *  |  _ <| | | | |/ _ \ |    | |/ _` | '_ \| __|
+ *  | |_) | | |_| |  __/ |____| | (_| | | | | |_
+ *  |____/|_|\__,_|\___|______|_|\__, |_| |_|\__|
+ *                                __/ |
+ *                               |___/
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @author BlueLightJapan Team
+ * 
+*/
+
+namespace pocketmine\item;
+ 
+abstract class Door extends Item{
+	public function getMaxStackSize(){
+		return 64;
+	}
+}
