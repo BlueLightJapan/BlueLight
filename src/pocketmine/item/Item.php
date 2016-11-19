bf5fc19a23d55b6ad55fc710d72d0570876a7bf4
KAMETAN0730 <kametan0730@gmail.com>
2016-11-19 18:52:26 +0900

    Update Item

--- src/pocketmine/item/Item.php
+++ src/pocketmine/item/Item.php
@@ -38,7 +38,7 @@ use pocketmine\Player;
 use pocketmine\Server;
 use pocketmine\utils\Config;
 
-class Item implements ItemIds{
+class Item implements ItemIds, \JsonSerializable{
 
 	/** @var NBT */
 	private static $cachedParser = null;
@@ -230,8 +230,12 @@ class Item implements ItemIds{
 
 		$creativeItems = new Config(Server::getInstance()->getFilePath() . "src/pocketmine/resources/creativeitems.json", Config::JSON, []);
 
-		foreach($creativeItems->getAll() as $item){
-			self::addCreativeItem(Item::get($item["ID"], $item["Damage"]));
+		foreach($creativeItems->getAll() as $data){
+			$item = Item::get($data["id"], $data["damage"], $data["count"], $data["nbt"]);
+			if($item->getName() === "Unknown"){
+				continue;
+			}
+			self::addCreativeItem($item);
 		}
 	}
 
@@ -244,7 +248,7 @@ class Item implements ItemIds{
 	}
 
 	public static function addCreativeItem(Item $item){
-		Item::$creative[] = Item::get($item->getId(), $item->getDamage());
+		Item::$creative[] = clone $item;
 	}
 
 	public static function removeCreativeItem(Item $item){
@@ -734,10 +738,6 @@ class Item implements ItemIds{
 		return false;
 	}
 
-	final public function __toString() : string{
-		return "Item " . $this->name . " (" . $this->id . ":" . ($this->meta === null ? "?" : $this->meta) . ")x" . $this->count . ($this->hasCompoundTag() ? " tags:0x" . bin2hex($this->getCompoundTag()) : "");
-	}
-
 	public function getDestroySpeed(Block $block, Player $player){
 		return 1;
 	}
@@ -758,6 +758,19 @@ class Item implements ItemIds{
 		}
 
 		return false;
+	}
+
+	final public function __toString() : string{
+		return "Item " . $this->name . " (" . $this->id . ":" . ($this->meta === null ? "?" : $this->meta) . ")x" . $this->count . ($this->hasCompoundTag() ? " tags:0x" . bin2hex($this->getCompoundTag()) : "");
+	}
+
+	final public function jsonSerialize(){
+		return [
+			"id" => $this->id,
+			"damage" => $this->meta,
+			"count" => $this->count, //TODO: separate items and stacks
+			"nbt" => $this->tags
+		];
 	}
 
 }
