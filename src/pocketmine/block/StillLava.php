38da1d2d936919dce6b9e7f86758653d73c93a12
KAMETAN0730 <kametan0730@gmail.com>
2016-12-21 16:34:26 +0900

    Add files via upload

--- src/pocketmine/block/StillLava.php
+++ src/pocketmine/block/StillLava.php
@@ -21,9 +21,19 @@
 
 namespace pocketmine\block;
 
+use pocketmine\level\Level;
+
 class StillLava extends Lava{
 
 	protected $id = self::STILL_LAVA;
+
+	public function onUpdate($type){
+		if($type !== Level::BLOCK_UPDATE_SCHEDULED){
+			return parent::onUpdate($type);
+		}else{
+			return false;
+		}
+	}
 
 	public function getName(){
 		return "Still Lava";
--- src/pocketmine/block/StillWater.php
+++ src/pocketmine/block/StillWater.php
@@ -21,9 +21,19 @@
 
 namespace pocketmine\block;
 
+use pocketmine\level\Level;
+
 class StillWater extends Water{
 
 	protected $id = self::STILL_WATER;
+
+	public function onUpdate($type){
+		if($type !== Level::BLOCK_UPDATE_SCHEDULED){
+			return parent::onUpdate($type);
+		}else{
+			return false;
+		}
+	}
 
 	public function getName(){
 		return "Still Water";
