db562fdc640e2827d83b6b898447e88ff0d2ffa6
KAMETAN0730 <kametan0730@gmail.com>
2016-12-22 20:05:17 +0900

    Revert "Better"

--- start.cmd
+++ start.cmd
@@ -1,5 +1,5 @@
 @echo off
-TITLE BlueLight Minecraft: Pocket Edition Server Software
+TITLE PocketMine-MP server software for Minecraft: Pocket Edition
 cd /d %~dp0
 
 if exist bin\php\php.exe (
@@ -9,28 +9,24 @@ if exist bin\php\php.exe (
 	set PHP_BINARY=php
 )
 
-if exist BlueLight*.phar (
-	set POCKETMINE_FILE=BlueLight*.phar
+if exist PocketMine-MP.phar (
+	set POCKETMINE_FILE=PocketMine-MP.phar
 ) else (
-	if exist PocketMine-MP.phar (
-		set POCKETMINE_FILE=PocketMine-MP.phar
+	if exist src\pocketmine\PocketMine.php (
+		set POCKETMINE_FILE=src\pocketmine\PocketMine.php
 	) else (
-	    if exist src\pocketmine\PocketMine.php (
-	        set POCKETMINE_FILE=src\pocketmine\PocketMine.php
-		) else (
-			if exist BlueLight-PHP7.phar (
-				set POCKETMINE_FILE=BlueLight-PHP7.phar
-			) else (
-		        echo "Couldn't find a valid installation."
-		        pause
-		        exit 8
-		    )
-	    )
+		echo "Couldn't find a valid PocketMine-MP installation"
+		pause
+		exit 1
 	)
 )
 
-if exist bin\mintty.exe (
-	start "" bin\mintty.exe -o Columns=88 -o Rows=32 -o AllowBlinking=0 -o FontQuality=3 -o Font="Consolas" -o FontHeight=10 -o CursorType=0 -o CursorBlinks=1 -h error -t "Genisys" -w max %PHP_BINARY% %POCKETMINE_FILE% --enable-ansi %*
-) else (
-	%PHP_BINARY% -c bin\php %POCKETMINE_FILE% %*
-)
+REM if exist bin\php\php_wxwidgets.dll (
+REM 	%PHP_BINARY% %POCKETMINE_FILE% --enable-gui %*
+REM ) else (
+	if exist bin\mintty.exe (
+		start "" bin\mintty.exe -o Columns=88 -o Rows=32 -o AllowBlinking=0 -o FontQuality=3 -o Font="DejaVu Sans Mono" -o FontHeight=10 -o CursorType=0 -o CursorBlinks=1 -h error -t "PocketMine-MP" -i bin/pocketmine.ico -w max %PHP_BINARY% %POCKETMINE_FILE% --enable-ansi %*
+	) else (
+		%PHP_BINARY% -c bin\php %POCKETMINE_FILE% %*
+	)
+REM )
