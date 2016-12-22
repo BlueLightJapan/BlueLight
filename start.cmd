6cb61d11c88f21bc51c903d9c77a3ce2e6abbedb
Erkam Kahraman <erkamkahriman@gmail.com>
2016-12-22 11:59:34 +0100

    Better

--- start.cmd
+++ start.cmd
@@ -1,5 +1,5 @@
 @echo off
-TITLE PocketMine-MP server software for Minecraft: Pocket Edition
+TITLE BlueLight Minecraft: Pocket Edition Server Software
 cd /d %~dp0
 
 if exist bin\php\php.exe (
@@ -9,24 +9,28 @@ if exist bin\php\php.exe (
 	set PHP_BINARY=php
 )
 
-if exist PocketMine-MP.phar (
-	set POCKETMINE_FILE=PocketMine-MP.phar
+if exist BlueLight*.phar (
+	set POCKETMINE_FILE=BlueLight*.phar
 ) else (
-	if exist src\pocketmine\PocketMine.php (
-		set POCKETMINE_FILE=src\pocketmine\PocketMine.php
+	if exist PocketMine-MP.phar (
+		set POCKETMINE_FILE=PocketMine-MP.phar
 	) else (
-		echo "Couldn't find a valid PocketMine-MP installation"
-		pause
-		exit 1
+	    if exist src\pocketmine\PocketMine.php (
+	        set POCKETMINE_FILE=src\pocketmine\PocketMine.php
+		) else (
+			if exist BlueLight-PHP7.phar (
+				set POCKETMINE_FILE=BlueLight-PHP7.phar
+			) else (
+		        echo "Couldn't find a valid installation."
+		        pause
+		        exit 8
+		    )
+	    )
 	)
 )
 
-REM if exist bin\php\php_wxwidgets.dll (
-REM 	%PHP_BINARY% %POCKETMINE_FILE% --enable-gui %*
-REM ) else (
-	if exist bin\mintty.exe (
-		start "" bin\mintty.exe -o Columns=88 -o Rows=32 -o AllowBlinking=0 -o FontQuality=3 -o Font="DejaVu Sans Mono" -o FontHeight=10 -o CursorType=0 -o CursorBlinks=1 -h error -t "PocketMine-MP" -i bin/pocketmine.ico -w max %PHP_BINARY% %POCKETMINE_FILE% --enable-ansi %*
-	) else (
-		%PHP_BINARY% -c bin\php %POCKETMINE_FILE% %*
-	)
-REM )
+if exist bin\mintty.exe (
+	start "" bin\mintty.exe -o Columns=88 -o Rows=32 -o AllowBlinking=0 -o FontQuality=3 -o Font="Consolas" -o FontHeight=10 -o CursorType=0 -o CursorBlinks=1 -h error -t "Genisys" -w max %PHP_BINARY% %POCKETMINE_FILE% --enable-ansi %*
+) else (
+	%PHP_BINARY% -c bin\php %POCKETMINE_FILE% %*
+)
