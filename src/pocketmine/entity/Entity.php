3db6940c18b48d01502776f472cd1e35a88164d6
nightmare3832 <nightmare3832@yahoo.co.jp>
2017-01-06 00:16:54 +0900

    Add some functions

--- src/pocketmine/entity/Entity.php
+++ src/pocketmine/entity/Entity.php
@@ -366,6 +366,30 @@ abstract class Entity extends Location implements Metadatable{
 		return $this->scale;
 	}
 
+	public function setHeight($height){
+		$this->height = $height;
+	}
+
+	public function getHeight(){
+		return $this->height;
+	}
+
+	public function setWidth($width){
+		$this->width = $width;
+	}
+
+	public function getWidth(){
+		return $this->width;
+	}
+
+	public function setLength($length){
+		$this->length = $length;
+	}
+
+	public function getLength(){
+		return $this->length;
+	}
+
 	/**
 	 * @return string
 	 */
