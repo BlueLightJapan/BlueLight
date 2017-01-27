a633521f64075a6f968ad0114b27620d99dcbb08
caspervanneck <wesleyvanneck1012@gmail.com>
2017-01-27 20:14:00 +0100

    Update EnderChest.php

--- src/pocketmine/block/EnderChest.php
+++ src/pocketmine/block/EnderChest.php
@@ -30,7 +30,7 @@ use pocketmine\nbt\tag\IntTag;
 use pocketmine\nbt\tag\ListTag;
 use pocketmine\nbt\tag\StringTag;
 use pocketmine\Player;
-use pocketmine\tile\Chest as TileChest;
+use pocketmine\tile\EnderChest;
 use pocketmine\tile\Tile;
 
 class EnderChest extends Transparent{
