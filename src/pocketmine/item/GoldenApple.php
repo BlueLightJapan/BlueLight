34dcc4ab88a659be906c1d390333c501d9163ba5
BlawoHD <blawohd@gmail.com>
2017-01-31 10:33:51 +0100

    Update GoldenApple

--- src/pocketmine/item/GoldenApple.php
+++ src/pocketmine/item/GoldenApple.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
  *
  *  ____            _        _   __  __ _                  __  __ ____
@@ -18,34 +17,34 @@
  *
  *
 */
-
 namespace pocketmine\item;
 
 use pocketmine\entity\Effect;
+use pocketmine\entity\Entity;
+use pocketmine\entity\Human;
 
 class GoldenApple extends Food{
+	
 	public function __construct($meta = 0, $count = 1){
-		parent::__construct(self::GOLDEN_APPLE, $meta, $count, ($meta === 1 ? "Enchanted " : "") . "Golden Apple");
+		parent::__construct(self::GOLDEN_APPLE, $meta, $count, "Golden Apple");
 	}
-
+	
+	public function canBeConsumedBy(Entity $entity) : bool{
+		return $entity instanceof Human;
+	}
+	
 	public function getFoodRestore() : int{
 		return 4;
 	}
-
+	
 	public function getSaturationRestore() : float{
 		return 9.6;
+		
 	}
-
 	public function getAdditionalEffects() : array{
-		return $this->meta === 1 ? [
-			Effect::getEffect(Effect::REGENERATION)->setDuration(600)->setAmplifier(4),
-			Effect::getEffect(Effect::ABSORPTION)->setDuration(2400),
-			Effect::getEffect(Effect::DAMAGE_RESISTANCE)->setDuration(6000),
-			Effect::getEffect(Effect::FIRE_RESISTANCE)->setDuration(6000),
-		] : [
+		return [
 			Effect::getEffect(Effect::REGENERATION)->setDuration(100)->setAmplifier(1),
 			Effect::getEffect(Effect::ABSORPTION)->setDuration(2400)
 		];
 	}
 }
-
