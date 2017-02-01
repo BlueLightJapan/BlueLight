1c0451851f8d5fbf718180c9b50596f7b3d4ea5a
caspervanneck <wesleyvanneck1012@gmail.com>
2017-02-01 15:53:11 +0100

    Oops rong branch

--- src/pocketmine/network/protocol/Info.php
+++ src/pocketmine/network/protocol/Info.php
@@ -30,8 +30,8 @@ interface Info{
 	/**
 	 * Actual Minecraft: PE protocol version
 	 */
-	const CURRENT_PROTOCOL = 101;
-	const ACCEPTED_PROTOCOLS = [101, 100];		
+	const CURRENT_PROTOCOL = 100;
+	const ACCEPTED_PROTOCOLS = [100];		
 				  	
 	const LOGIN_PACKET = 0x01;
 	const PLAY_STATUS_PACKET = 0x02;
