8278f33c7eb2eaa568437dbdd653ca81f2e63025
BlawoHD <blawohd@gmail.com>
2017-02-03 10:32:27 +0100

    Update TransferCommand.php

--- src/pocketmine/command/defaults/TransferCommand.php
+++ src/pocketmine/command/defaults/TransferCommand.php
@@ -8,29 +8,27 @@ use pocketmine\command\Command;
 use pocketmine\Server;
 use pocketmine\Player;
 
-class TransferCommand extends VanillaCommand{ 
-    public function __construct($name)
-    {
-        
+class TransferCommand extends VanillaCommand{
+	
+    public function __construct($name){
         parent::__construct(
         $name,
         "%pocketmine.command.transfer.description",
         "%pocketmine.command.transfer.usage",
         ["transfer"]
-    );
+        );
         $this->setPermission("pocketmine.command.transfer");
     }
+
     public function execute(CommandSender $sender, $currentAlias, array $args){
-        if(!($this->testPermission($sender))){
-            return false;
-        }
 		if($sender instanceof Player){
         	$pk = new TransferPacket();
         	$pk->address = $args[0];
         	$pk->port = $args[1];
         	$sender->dataPacket($pk);
+
         	Command::broadcastCommandMessage($sender, "Transferred to " . $args[0] . ":" . $args[1]);
-		} else {
+		}else{
 			$sender->sendMessage("Run this command in game!");
 		}
     }
