697b658e4ab3287100b20910cd090f404dd4d7da
caspervanneck <wesleyvanneck1012@gmail.com>
2017-02-25 16:55:22 +0100

    bumb it upp

--- src/pocketmine/PocketMine.php
+++ src/pocketmine/PocketMine.php
@@ -76,8 +76,8 @@ namespace pocketmine {
 	const VERSION = "2.0dev";
 	const API_VERSION = "3.0.0";
 	const CODENAME = "PocketMine-MP-DerivedVersion";
-	const MINECRAFT_VERSION = "v1.0.4 alpha";
-	const MINECRAFT_VERSION_NETWORK = "1.0.4";
+	const MINECRAFT_VERSION = "v1.0.4.1 alpha";
+	const MINECRAFT_VERSION_NETWORK = "1.0.4.1";
 	const BLUELIGHT_VERSION = "2.0.0";
 
 	/*
