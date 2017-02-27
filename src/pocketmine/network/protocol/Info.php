1296180a8a7007186b097306f544e480f53eb1ee
caspervanneck <wesleyvanneck1012@gmail.com>
2017-02-27 15:25:51 +0100

    add that back not [working jet]

--- src/pocketmine/network/protocol/Info.php
+++ src/pocketmine/network/protocol/Info.php
@@ -30,7 +30,8 @@ interface Info{
 	/**
 	 * Actual Minecraft: PE protocol version
 	 */
-	const CURRENT_PROTOCOL = 102;
+	const CURRENT_PROTOCOL = 101;
+	const ACCEPTED_PROTOCOLS = [100, 101, 102];
 
 	const LOGIN_PACKET = 0x01;
 	const PLAY_STATUS_PACKET = 0x02;
