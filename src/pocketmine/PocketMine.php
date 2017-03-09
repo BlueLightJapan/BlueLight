10b687e2a53d40277d4cda027e9d40257b5ffe7d
BlawoHD <blawohd@gmail.com>
2017-03-09 18:28:56 +0100

    1.0.4 Version is out!

--- src/pocketmine/PocketMine.php
+++ src/pocketmine/PocketMine.php
@@ -76,8 +76,8 @@ namespace pocketmine {
 	const VERSION = "2.0dev";
 	const API_VERSION = "3.0.0-ALPHA4";
 	const CODENAME = "BlueLight";
-	const MINECRAFT_VERSION = "v1.0.3 alpha";
-	const MINECRAFT_VERSION_NETWORK = "1.0.3";
+	const MINECRAFT_VERSION = "v1.0.4 alpha";
+	const MINECRAFT_VERSION_NETWORK = "1.0.4";
 	const BLUELIGHT_VERSION = "2.0.0";
 
 	/*
