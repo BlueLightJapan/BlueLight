fb354ea661d642f0819951b89cee162a4da5288b
BlawoHD <blawohd@gmail.com>
2017-04-20 22:51:42 +0200

    Update Block.php

--- src/pocketmine/block/Block.php
+++ src/pocketmine/block/Block.php
@@ -253,6 +253,7 @@ class Block extends Position implements BlockIds, Metadatable{
 			self::$list[self::GLOWING_OBSIDIAN] = GlowingObsidian::class;
 
 			self::$list[self::HOPPER_BLOCK] = Hopper::class;
+		        self::$list[self::DRAGON_EGG] = DragonEgg::class;
 			foreach(self::$list as $id => $class){
 				if($class !== null){
 					/** @var Block $block */
