d41d6b7d1d461b409a839bac5eb644acb08f01fc
BlawoHD <blawohd@gmail.com>
2017-04-21 01:31:34 +0200

    Create BeaconInventory.php

---
+++ src/pocketmine/inventory/BeaconInventory.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ *
+ *    _______                                _
+ *   |__   __|                              | |
+ *      | | ___  ___ ___  ___ _ __ __ _  ___| |_
+ *      | |/ _ \/ __/ __|/ _ \  __/ _` |/ __| __|
+ *      | |  __/\__ \__ \  __/ | | (_| | (__| |_
+ *      |_|\___||___/___/\___|_|  \__,_|\___|\__|
+ *
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @author Tessetact Team
+ * @link http://www.github.com/TesseractTeam/Tesseract
+ * 
+ *
+ */
+ 
+namespace pocketmine\inventory;
+
+use pocketmine\tile\Beacon;
+
+class BeaconInventory extends ContainerInventory {
+	
+	public function __construct(Beacon $tile){
+		parent::__construct($tile, InventoryType::get(InventoryType::BEACON));
+	}
+	
+	public function getHolder() {
+		return $this->holder;
+	}
+}
