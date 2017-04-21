0f1991ce7eab35e2b7374717a839d074d1e4079e
BlawoHD <blawohd@gmail.com>
2017-04-21 15:28:52 +0200

    Add ChorusFlower

---
+++ src/pocketmine/block/ChorusFlower.php
@@ -0,0 +1,55 @@
+<?php
+
+/*
+ *
+ *  ____            _        _   __  __ _                  __  __ ____
+ * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \
+ * | |_) / _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \_____| |\/| | |_) |
+ * |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/
+ * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_|
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @author PocketMine Team
+ * @link http://www.pocketmine.net/
+ *
+ *
+*/
+
+namespace pocketmine\block;
+
+use pocketmine\item\Item;
+use pocketmine\item\Tool;
+
+class ChorusFlower extends Solid{
+
+	protected $id = self::CHORUS_FLOWER;
+  
+	public function __construct($meta = 0){
+		$this->meta = $meta;
+	}
+  
+ 	public function getHardness(){
+		return 0.4;
+	}
+  
+	public function getToolType(){
+		return Tool::TYPE_AXE;
+	}
+  
+	public function getName() : string{
+		return "Chorus Flower";
+	}
+  
+	public function getDrops(Item $item) : array {
+		$drops = [];
+		if($this->meta >= 0x07){
+			$drops[] = [Item::CHORUS_FRUIT, 0, 1];
+		}
+		return $drops;
+	}
+  
+}
