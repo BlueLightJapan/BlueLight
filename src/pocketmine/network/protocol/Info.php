91fe8452cacea64c8495ed9b7d0e0c6b80f038fd
BlawoHD <blawohd@gmail.com>
2017-04-21 12:07:09 +0200

    Update Info.php

--- src/pocketmine/network/protocol/Info.php
+++ src/pocketmine/network/protocol/Info.php
@@ -31,7 +31,7 @@ interface Info{
 	 * Actual Minecraft: PE protocol version
 	 */
 	const CURRENT_PROTOCOL = 107;
-	const ACCEPTED_PROTOCOLS = [106, 107];
+	const ACCEPTED_PROTOCOLS = [107];
 
 	const LOGIN_PACKET = 0x01;
 	const PLAY_STATUS_PACKET = 0x02;
