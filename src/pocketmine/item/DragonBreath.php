268bbf6efbbf7ada340db93ed74bb0294faa5fbd
BlawoHD <blawohd@gmail.com>
2017-04-21 16:03:36 +0200

    Update and rename DragonsBreath.php to DragonBreath.php

--- src/pocketmine/item/DragonBreath.php
+++ src/pocketmine/item/DragonBreath.php
@@ -21,9 +21,9 @@
 
 namespace pocketmine\item;
 
-class DragonsBreath extends Item{
+class DragonBreath extends Item{
 	public function __construct($meta = 0, $count = 1){
-		parent::__construct(self::DRAGONS_BREATH, 0, $count, "Dragon's Breath");
+		parent::__construct(self::DRAGON_BREATH, 0, $count, "Dragon's Breath");
 	}
   
 }
