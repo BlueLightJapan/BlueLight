4bbaae2a6f260a5cd26242f56d67994ffe651a98
BlawoHD <blawohd@gmail.com>
2017-04-21 15:57:24 +0200

    Add TitleCommand

---
+++ src/pocketmine/command/defaults/TitleCommand.php
@@ -0,0 +1,49 @@
+<?php
+
+/*
+ *
+ *  ____            _        _   __  __ _                  __  __ ____
+ * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \
+ * | |_) / _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \_____| |\/| | |_) |
+ * |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/
+ * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_|
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @author PocketMine Team
+ * @link http://www.pocketmine.net/
+ *
+ *
+*/
+ 
+namespace pocketmine\command\defaults;
+ 
+use pocketmine\network\protocol\SetTitlePacket;
+use pocketmine\command\CommandSender;
+
+class TitleCommand extends VanillaCommand {
+
+	public function __construct($name){
+		parent::__construct(
+			$name,
+			"%pocketmine.command.title.description",
+			"%pocketmine.command.title.usage"
+		);
+		$this->setPermission("pocketmine.command.title");
+	}
+  
+	public function execute(CommandSender $sender, $currentAlias, array $args){
+		if($sender instanceof Player){
+			if(!$this->testPermission($sender)){
+				return true;
+			}
+			if(count($args) <= 0){
+				$sender->sendMessage("Usage: /title <title> <subtile> [text]");
+				return false;
+        }
+    }
+    
+}
