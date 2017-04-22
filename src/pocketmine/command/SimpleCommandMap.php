e8bfa7127e9ddc3b0becf6a3bb5a0f2adc9f7dcb
KAMETAN0730 <kametan0730@gmail.com>
2017-04-22 20:51:31 +0900

    Add title command

--- src/pocketmine/command/SimpleCommandMap.php
+++ src/pocketmine/command/SimpleCommandMap.php
@@ -57,6 +57,7 @@ use pocketmine\command\defaults\TeleportCommand;
 use pocketmine\command\defaults\TellCommand;
 use pocketmine\command\defaults\TimeCommand;
 use pocketmine\command\defaults\TimingsCommand;
+use pocketmine\command\defaults\TitleCommand;
 use pocketmine\command\defaults\VanillaCommand;
 use pocketmine\command\defaults\VersionCommand;
 use pocketmine\command\defaults\WhitelistCommand;
@@ -119,6 +120,7 @@ class SimpleCommandMap implements CommandMap{
 		$this->register("pocketmine", new TeleportCommand("tp"));
 		$this->register("pocketmine", new TimeCommand("time"));
 		$this->register("pocketmine", new TimingsCommand("timings"));
+		$this->register("pocketmine", new TitleCommand("title"));
 		$this->register("pocketmine", new ReloadCommand("reload"));
 		$this->register("pocketmine", new WeatherCommand("weather"));
 		$this->register("pocketmine", new TransferCommand("transfer"));
