3b1f24220c78e4a1bfcc3219dc0150366419309a
Driesboy <coenendries3@gmail.com>
2017-04-22 12:40:02 +0200

    Better transfer command

--- src/pocketmine/command/defaults/TransferCommand.php
+++ src/pocketmine/command/defaults/TransferCommand.php
@@ -22,37 +22,49 @@ namespace pocketmine\command\defaults;
 
 use pocketmine\network\protocol\TransferPacket;
 use pocketmine\command\CommandSender;
-use pocketmine\command\Command;
-use pocketmine\Server;
-use pocketmine\Player;
+use pocketmine\{Player, Server};
 
 class TransferCommand extends VanillaCommand{
 	
 	public function __construct($name){
 		parent::__construct(
-		$name,
-		"%pocketmine.command.transfer.description",
-		"%pocketmine.command.transfer.usage",
-		["transfer","connect"]
+			$name,
+			"%pocketmine.command.transfer.description",
+			"%pocketmine.command.transfer.usage",
+			["transfer"]
 		);
 		$this->setPermission("pocketmine.command.transfer");
 	}
-
+	
 	public function execute(CommandSender $sender, $currentAlias, array $args){
-		if(!($this->testPermission($sender))) return;
+		$address = null;
+		$port = null;
+		$player = null;
 		if($sender instanceof Player){
-			$port = 19132;
-			if(!(empty($args[1]))) $port = $args[1];
-			if(empty($args[0])) return $sender->sendMessage("Usage: /transfer <ip> [port]");
-
-			$pk = new TransferPacket();
-			$pk->address = $args[0];
-			$pk->port = $port;
-			$sender->dataPacket($pk);
-
-			Command::broadcastCommandMessage($sender, "Transferred to " . $args[0] . ":" . $port);
-		}else{
-			$sender->sendMessage("Run this command in game!");
+			if(!$this->testPermission($sender)){
+				return true;
+			}
+			if(count($args) <= 0){
+				$sender->sendMessage("Usage: /transferserver <address> [port]");
+				return false;
+			}
+			$address = strtolower($args[0]);
+			$port = (isset($args[1]) && is_numeric($args[1]) ? $args[1] : 19132);
+			$sender->transfer($address, $port);
+			return false;
 		}
+		if(count($args) <= 1){
+			$sender->sendMessage("Usage: /transferserver <player> <address> [port]");
+			return false;
+		}
+		if(!($player = Server::getInstance()->getPlayer($args[0])) instanceof Player){
+			$sender->sendMessage("Player specified not found!");
+			return false;
+		}
+		$address = strtolower($args[1]);
+		$port = (isset($args[2]) && is_numeric($args[2]) ? $args[2] : 19132);
+		
+		$sender->sendMessage("Sending ".$player->getName()." to ".$address.":".$port);
+		$player->transfer($address, $port);
 	}
 }
