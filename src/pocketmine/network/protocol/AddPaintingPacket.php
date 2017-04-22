76b0d917f25da30c643ac8058d0b0a4b7a4564bc
BlawoHD <blawohd@gmail.com>
2017-04-22 23:03:56 +0200

    Update AddPaintingPacket.php

--- src/pocketmine/network/protocol/AddPaintingPacket.php
+++ src/pocketmine/network/protocol/AddPaintingPacket.php
@@ -23,21 +23,19 @@ namespace pocketmine\network\protocol;
 
 #include <rules/DataPacket.h>
 
-
 class AddPaintingPacket extends DataPacket{
+	
 	const NETWORK_ID = Info::ADD_PAINTING_PACKET;
-
 	public $eid;
 	public $x;
 	public $y;
 	public $z;
 	public $direction;
 	public $title;
-
+	
 	public function decode(){
-
 	}
-
+	
 	public function encode(){
 		$this->reset();
 		$this->putEntityId($this->eid); //EntityUniqueID
@@ -46,5 +44,10 @@ class AddPaintingPacket extends DataPacket{
 		$this->putVarInt($this->direction);
 		$this->putString($this->title);
 	}
-
+	/**
+	 * @return string
+         */
+	public function getName(){
+		return "AddPaintingPacket";
+	}
 }
