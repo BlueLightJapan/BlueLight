b16bcecde5025cca9f6af48ee65914afbf531c35
Kametan0730 <kametan@kametan.tokyo>
2017-04-23 08:35:33 +0900

    Revert "Update MonsterSpawner.php"
    This reverts commit 65d1a49ef6cff5f498e2a826ee3dd87298130ba7.

--- src/pocketmine/block/MonsterSpawner.php
+++ src/pocketmine/block/MonsterSpawner.php
@@ -2,7 +2,7 @@
 
 /*
  *
- *  ____            _        _   __  __ _                  __  __ ____ 
+ *  ____            _        _   __  __ _                  __  __ ____
  * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \
  * | |_) / _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \_____| |\/| | |_) |
  * |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/
@@ -23,74 +23,28 @@ namespace pocketmine\block;
 
 use pocketmine\item\Item;
 use pocketmine\item\Tool;
-use pocketmine\nbt\tag\CompoundTag;
-use pocketmine\nbt\tag\IntTag;
-use pocketmine\nbt\tag\StringTag;
-use pocketmine\tile\Tile;
-use pocketmine\tile\MobSpawner;
-use pocketmine\Player;
 
 class MonsterSpawner extends Solid{
-	
+
 	protected $id = self::MONSTER_SPAWNER;
-	
+
 	public function __construct($meta = 0){
 		$this->meta = $meta;
 	}
-	
-	public function getHardness() {
+
+	public function getHardness(){
 		return 5;
 	}
-	
+
 	public function getToolType(){
 		return Tool::TYPE_PICKAXE;
 	}
-	
-	public function getName() : string{
+
+	public function getName(){
 		return "Monster Spawner";
 	}
-	
-	public function canBeActivated() : bool {
-		return true;
-	}
-	
-	public function onActivate(Item $item, Player $player = null){
-		if($this->getDamage() == 0){
-			if($item->getId() == Item::SPAWN_EGG){
-				$tile = $this->getLevel()->getTile($this);
-				if($tile instanceof MobSpawner){
-					$this->meta = $item->getDamage();
-					//$this->getLevel()->setBlock($this, $this, true, false);
-					$tile->setEntityId($this->meta);
-				}
-				return true;
-			}
-		}
-		return false;
-	}
-	
-	public function place(Item $item, Block $block, Block $target, $face, $fx, $fy, $fz, Player $player = null){
-		$this->getLevel()->setBlock($block, $this, true, true);
-		$nbt = new CompoundTag("", [
-			new StringTag("id", Tile::MOB_SPAWNER),
-			new IntTag("x", $block->x),
-			new IntTag("y", $block->y),
-			new IntTag("z", $block->z),
-			new IntTag("EntityId", 0),
-		]);
-		
-		if($item->hasCustomBlockData()){
-			foreach($item->getCustomBlockData() as $key => $v){
-				$nbt->{$key} = $v;
-			}
-		}
-		
-		Tile::createTile(Tile::MOB_SPAWNER, $this->getLevel(), $nbt);
-		return true;
-	}
-	
-	public function getDrops(Item $item) : array {
+
+	public function getDrops(Item $item){
 		return [];
 	}
-	
 }
