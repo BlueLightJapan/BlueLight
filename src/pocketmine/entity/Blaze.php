99cbc7dfffa2d5f6e4237d014cc79bdc9357e4d4
KAMETAN0730 <kametan0730@gmail.com>
2017-04-29 13:50:26 +0900

    Add  ASCII art Logo

--- src/pocketmine/entity/Blaze.php
+++ src/pocketmine/entity/Blaze.php
@@ -1,23 +1,23 @@
 <?php
 
 /*
- *
- *  _____   _____   __   _   _   _____  __    __  _____
- * /  ___| | ____| |  \ | | | | /  ___/ \ \  / / /  ___/
- * | |     | |__   |   \| | | | | |___   \ \/ /  | |___
- * | |  _  |  __|  | |\   | | | \___  \   \  /   \___  \
- * | |_| | | |___  | | \  | | |  ___| |   / /     ___| |
- * \_____/ |_____| |_|  \_| |_| /_____/  /_/     /_____/
- *
+ *   ____  _            _      _       _     _
+ *  |  _ \| |          | |    (_)     | |   | |
+ *  | |_) | |_   _  ___| |     _  __ _| |__ | |_
+ *  |  _ <| | | | |/ _ \ |    | |/ _` | '_ \| __|
+ *  | |_) | | |_| |  __/ |____| | (_| | | | | |_
+ *  |____/|_|\__,_|\___|______|_|\__, |_| |_|\__|
+ *                                __/ |
+ *                               |___/
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * @author iTX Technologies
- * @link https://itxtech.org
- *
- */
+ * @author BlueLightJapan Team
+ * 
+*/
+
 
 namespace pocketmine\entity;
 
@@ -46,6 +46,7 @@ class Blaze extends Monster{
 	private $heightOffsetUpdateTime;
 
 	public function initEntity(){
+
 		$this->tasks->addTask(5, new EntityAIMoveTowardsRestriction($this, 1.0));
 		$this->tasks->addTask(7, new EntityAIWander($this, 1.0));
 		$this->tasks->addTask(8, new EntityAIWatchClosest($this, "pocketmine\Player", 8.0));
