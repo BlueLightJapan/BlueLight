7be148f3f401520b76063e07ad77d3200e21fed0
poustmal <poustmal12@gmail.com>
2017-05-30 13:49:10 +0300

    Add possibly config for nether (unfunctionable)

--- src/pocketmine/Server.php
+++ src/pocketmine/Server.php
@@ -259,6 +259,8 @@ class Server{
 	public $lightningTime = 200;
 	public $lightningFire = false;
 	public $foodEnabled = false;
+ public $netherEnabled = false;
+	public $netherName = "nether";
 	public $expEnabled = false;
 	public $hungerHealth = 10;
 	public $hungerTimer = 80;
