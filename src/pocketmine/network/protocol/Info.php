dcf4634a28031e24f95694ee5834560db9866b05
shoki <k.shoki@outlook.jp>
2017-06-02 01:50:55 +0900

    ver1.1 protocol?

--- src/pocketmine/network/protocol/Info.php
+++ src/pocketmine/network/protocol/Info.php
@@ -30,8 +30,8 @@ interface Info{
 	/**
 	 * Actual Minecraft: PE protocol version
 	 */
-	const CURRENT_PROTOCOL = 107;
-	const ACCEPTED_PROTOCOLS = [105, 106, 107];
+	const CURRENT_PROTOCOL = 110;
+	const ACCEPTED_PROTOCOLS = [107, 110];
 
 	const LOGIN_PACKET = 0x01;
 	const PLAY_STATUS_PACKET = 0x02;
