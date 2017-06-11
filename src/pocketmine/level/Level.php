f63a150eb049b86aa3f15bbbb6fdea3f9e6b4fda
ken77731 <kento010811@gmail.com>
2017-06-11 20:52:27 +0900

    Add files via upload

--- src/pocketmine/level/Level.php
+++ src/pocketmine/level/Level.php
@@ -2419,6 +2419,10 @@ class Level implements ChunkManager, Metadatable{
 		return Position::fromObject($this->provider->getSpawn(), $this);
 	}
 
+	public function getSpawn(){
+		return $this->getSpawnLocation();
+	}
+
 	/**
 	 * Sets the level spawn location
 	 *
