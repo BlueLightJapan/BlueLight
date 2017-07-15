fd3356a16002c0d922362170a1913152956c5b35
yuko fuyutsuki <yukofuyutsuki@gmail.com>
2017-07-16 08:22:59 +0900

    Working Beacon!
    * Thanks to part of code by Tesseract-Team.

--- src/pocketmine/block/Beacon.php
+++ src/pocketmine/block/Beacon.php
@@ -15,11 +15,11 @@
  * (at your option) any later version.
  *
  * @author BlueLightJapan Team
- * 
  */
 
- 
+
 namespace pocketmine\block;
+
 use pocketmine\item\Item;
 use pocketmine\Player;
 use pocketmine\nbt\tag\CompoundTag;
@@ -28,84 +28,92 @@ use pocketmine\nbt\tag\IntTag;
 use pocketmine\nbt\tag\StringTag;
 use pocketmine\tile\Tile;
 use pocketmine\tile\Beacon as TileBeacon;
+
 class Beacon extends Transparent{
- 
- 	protected $id = self::BEACON;
- 
- 	public function __construct($meta = 0){
- 		$this->meta = $meta;
- 	}
- 
- 	public function canBeActivated() : bool{
- 		return true;
- 	}
- 
- 	public function getName(){
- 		return "Beacon";
- 	}
-	
+
+	protected $id = self::BEACON;
+
+	public function __construct($meta = 0){
+		$this->meta = $meta;
+	}
+
+	/**
+	 * @return bool
+	 */
+	public function canBeActivated() : bool{
+		return true;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getName(): string{
+		return "Beacon";
+	}
+
 	public function getLightLevel(){
 		return 15;
 	}
-	
+
 	public function getResistance() {
 		return 15;
 	}
-	
+
 	public function getHardness() {
 		return 3;
 	}
- 
- 	public function place(Item $item, Block $block, Block $target, $face, $fx, $fy, $fz, Player $player = null){
- 		$this->getLevel()->setBlock($this, $this, true, true);
- 		$nbt = new CompoundTag("", [
- 			new StringTag("id", Tile::BEACON),
+
+	public function place(Item $item, Block $block, Block $target, $face, $fx, $fy, $fz, Player $player = null){
+		$this->getLevel()->setBlock($this, $this, true, true);
+		$nbt = new CompoundTag("", [
+			new StringTag("id", Tile::BEACON),
 			new ByteTag("isMovable", (bool) false),
 			new IntTag("primary", 0),
 			new IntTag("secondary", 0),
- 			new IntTag("x", $block->x),
- 			new IntTag("y", $block->y),
- 			new IntTag("z", $block->z)
- 		]);
- 		$pot = Tile::createTile(Tile::BEACON, $this->getLevel(), $nbt);
- 		return true;
- 	}
-	
+			new IntTag("x", $block->x),
+			new IntTag("y", $block->y),
+			new IntTag("z", $block->z)
+		]);
+		$pot = Tile::createTile(Tile::BEACON, $this->getLevel(), $nbt);
+		return true;
+	}
+
 	public function onActivate(Item $item, Player $player = null){
- 		if($player instanceof Player){
- 			$top = $this->getSide(1);
- 			if($top->isTransparent() !== true){
-				return true;
- 			}
- 
-			$t = $this->getLevel()->getTile($this);
- 			$beacon = null;
- 			if($t instanceof TileBeacon){
- 				$beacon = $t;
- 			}else{
- 				$nbt = new CompoundTag("", [
- 					new StringTag("id", Tile::BEACON),
- 					new ByteTag("isMovable", (bool) false),
- 					new IntTag("primary", 0),
- 					new IntTag("secondary", 0),
- 					new IntTag("x", $this->x),
- 					new IntTag("y", $this->y),
- 					new IntTag("z", $this->z)
- 				]);
- 				Tile::createTile(Tile::BEACON, $this->getLevel(), $nbt);
- 			}
-			
-			if($player->isCreative()){
+		if($player instanceof Player){
+			$top = $this->getSide(1);
+			if($top->isTransparent() !== true){
 				return true;
 			}
- 				$player->addWindow($beacon->getInventory());
- 		}
- 
- 		return true;
- 	}
-	
+
+			$t = $this->getLevel()->getTile($this);
+			$beacon = null;
+			if($t instanceof TileBeacon){
+				$beacon = $t;
+			}else{
+				$nbt = new CompoundTag("", [
+					new StringTag("id", Tile::BEACON),
+					new ByteTag("isMovable", (bool) false),
+					new IntTag("primary", 0),
+					new IntTag("secondary", 0),
+					new IntTag("x", $this->x),
+					new IntTag("y", $this->y),
+					new IntTag("z", $this->z)
+				]);
+				Tile::createTile(Tile::BEACON, $this->getLevel(), $nbt);
+			}
+			$player->addWindow($beacon->getInventory());
+		}
+
+		return true;
+	}
+
+	/**
+	 * @param Item $item
+	 *
+	 * @return bool
+	 */
 	public function onBreak(Item $item){
 		$this->getLevel()->setBlock($this, new Air(), true, true);
 		return true;
 	}
- }
+}
--- src/pocketmine/inventory/InventoryType.php
+++ src/pocketmine/inventory/InventoryType.php
@@ -41,6 +41,11 @@ class InventoryType{
 	const BREWING_STAND = 7;
 	const ANVIL = 8;
 	const ENCHANT_TABLE = 9;
+	const DISPENSER = 10;
+	const DROPPER = 11;
+	const HOPPER = 12;
+	const ENDER_CHEST = 13;
+	const BEACON = 14;
 
 	private static $default = [];
 
@@ -65,15 +70,34 @@ class InventoryType{
 		//TODO: move network stuff out of here
 		//TODO: move inventory data to json
 		static::$default = [
-			static::CHEST =>         new InventoryType(27, "Chest", WindowTypes::CONTAINER),
-			static::DOUBLE_CHEST =>  new InventoryType(27 + 27, "Double Chest", WindowTypes::CONTAINER),
-			static::PLAYER =>        new InventoryType(36 + 4, "Player", WindowTypes::INVENTORY), //36 CONTAINER, 4 ARMOR
-			static::CRAFTING =>      new InventoryType(5, "Crafting", WindowTypes::INVENTORY), //yes, the use of INVENTORY is intended! 4 CRAFTING slots, 1 RESULT
-			static::WORKBENCH =>     new InventoryType(10, "Crafting", WindowTypes::WORKBENCH), //9 CRAFTING slots, 1 RESULT
-			static::FURNACE =>       new InventoryType(3, "Furnace", WindowTypes::FURNACE), //2 INPUT, 1 OUTPUT
-			static::ENCHANT_TABLE => new InventoryType(2, "Enchant", WindowTypes::ENCHANTMENT), //1 INPUT/OUTPUT, 1 LAPIS
-			static::BREWING_STAND => new InventoryType(4, "Brewing", WindowTypes::BREWING_STAND), //1 INPUT, 3 POTION
-			static::ANVIL =>         new InventoryType(3, "Anvil", WindowTypes::ANVIL) //2 INPUT, 1 OUTP
+			static::CHEST
+			=> new InventoryType(27, "Chest", WindowTypes::CONTAINER),
+			static::DOUBLE_CHEST
+			=> new InventoryType(27 + 27, "Double Chest", WindowTypes::CONTAINER),
+			static::PLAYER
+			=> new InventoryType(36 + 4, "Player", WindowTypes::INVENTORY), //36 CONTAINER, 4 ARMOR
+			static::CRAFTING
+			=> new InventoryType(5, "Crafting", WindowTypes::INVENTORY), //yes, the use of INVENTORY is intended! 4 CRAFTING slots, 1 RESULT
+			static::WORKBENCH
+			=> new InventoryType(10, "Crafting", WindowTypes::WORKBENCH), //9 CRAFTING slots, 1 RESULT
+			static::FURNACE
+			=> new InventoryType(3, "Furnace", WindowTypes::FURNACE), //2 INPUT, 1 OUTPUT
+			static::ENCHANT_TABLE
+			=> new InventoryType(2, "Enchant", WindowTypes::ENCHANTMENT), //1 INPUT/OUTPUT, 1 LAPIS
+			static::BREWING_STAND
+			=> new InventoryType(4, "Brewing", WindowTypes::BREWING_STAND), //1 INPUT, 3 POTION
+			static::ANVIL
+			=> new InventoryType(3, "Anvil", WindowTypes::ANVIL), //2 INPUT, 1 OUTPUT
+			static::DISPENSER
+			=> new InventoryType(9, "Dispenser", WindowTypes::DISPENSER), //9 CONTAINER
+			static::DROPPER
+			=> new InventoryType(9, "Dropper", WindowTypes::DROPPER), //9 CONTAINER
+			static::HOPPER
+			=> new InventoryType(5, "Hopper", WindowTypes::HOPPER), //5 CONTAINER
+			static::ENDER_CHEST
+			=> new InventoryType(27, "Ender Chest", WindowTypes::CONTAINER),
+			static::BEACON
+			=> new InventoryType(0, "Beacon", WindowTypes::BEACON),
 		];
 	}
 
