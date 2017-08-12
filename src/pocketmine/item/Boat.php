89bbc1fbf0b0ccbd5addbe2e68ccf79b6f7af23e
KAMETAN0730 <kametan0730@gmail.com>
2017-08-12 09:54:41 +0900

    Update Boat

--- src/pocketmine/item/Boat.php
+++ src/pocketmine/item/Boat.php
@@ -27,4 +27,36 @@ class Boat extends Item{
 	public function __construct($meta = 0, $count = 1){
 		parent::__construct(self::BOAT, $meta, $count, "Boat");
 	}
+	public function onActivate(Level $level, Player $player, Block $block, Block $target, $face, $fx, $fy, $fz){
+		$realPos = $block->getSide($face);
+		$boat = new BoatEntity($player->getLevel(), new CompoundTag("", [
+			"Pos" => new ListTag("Pos", [
+				new DoubleTag("", $realPos->getX() + 0.5),
+				new DoubleTag("", $realPos->getY()),
+				new DoubleTag("", $realPos->getZ() + 0.5)
+			]),
+			"Motion" => new ListTag("Motion", [
+				new DoubleTag("", 0),
+				new DoubleTag("", 0),
+				new DoubleTag("", 0)
+			]),
+			"Rotation" => new ListTag("Rotation", [
+				new FloatTag("", 0),
+				new FloatTag("", 0)
+			]),
+			"WoodID" => new IntTag("WoodID", $this->getDamage())
+		]));
+		$boat->spawnToAll();
+		if($player->isSurvival()) {
+			$item = $player->getInventory()->getItemInHand();
+			$count = $item->getCount();
+			if(--$count <= 0){
+				$player->getInventory()->setItemInHand(Item::get(Item::AIR));
+				return true;
+			}
+			$item->setCount($count);
+			$player->getInventory()->setItemInHand($item);
+		}
+		return true;
+	}
 }
