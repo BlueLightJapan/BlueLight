3f0c0dfc19d1bd1cc24e9af6572050faa7bb9032
kametan0730 <kametan@kametan.tokyo>
2017-09-24 19:07:30 +0900

    Fixed Minecart

--- src/pocketmine/block/PoweredRail.php
+++ src/pocketmine/block/PoweredRail.php
@@ -1,32 +1,37 @@
 <?php
 
 /*
- *
- *  ____            _        _   __  __ _                  __  __ ____
- * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \
- * | |_) / _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \_____| |\/| | |_) |
- * |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/
- * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_|
- *
+ *   ____  _            _      _       _     _
+ *  |  _ \| |          | |    (_)     | |   | |
+ *  | |_) | |_   _  ___| |     _  __ _| |__ | |_
+ *  |  _ <| | | | |/ _ \ |    | |/ _` | '_ \| __|
+ *  | |_) | | |_| |  __/ |____| | (_| | | | | |_
+ *  |____/|_|\__,_|\___|______|_|\__, |_| |_|\__|
+ *                                __/ |
+ *                               |___/
  * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
+ * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * @author PocketMine Team
- * @link http://www.pocketmine.net/
- *
- *
+ * @author BlueLightJapan Team
+ * 
 */
-
-declare(strict_types=1);
 
 namespace pocketmine\block;
 
 class PoweredRail extends Rail{
 	protected $id = self::POWERED_RAIL;
 
+	public function __construct($meta = 0){
+		$this->meta = $meta;
+	}
+
 	public function getName() : string{
 		return "Powered Rail";
+	}
+
+	public function isPowered(){
+		return true;//TODO
 	}
 }
