7900b6b76e209f355ee809a192481f059d8c8d63
BlawoHD <blawohd@gmail.com>
2017-09-27 13:13:36 +0200

    Update InventoryTransactionPacket.php

--- src/pocketmine/network/mcpe/protocol/InventoryTransactionPacket.php
+++ src/pocketmine/network/mcpe/protocol/InventoryTransactionPacket.php
@@ -50,6 +50,13 @@ class InventoryTransactionPacket extends DataPacket{
 
 	/** @var int */
 	public $transactionType;
+	
+	/**
+ 	 * @var bool
+ 	 * NOTE: THIS FIELD DOES NOT EXIST IN THE PROTOCOL, it's merely used for convenience for PocketMine-MP to easily
+ 	 * determine whether we're doing a crafting transaction.
+ 	 */
+ 	public $isCraftingPart = false;
 
 	/** @var NetworkInventoryAction[] */
 	public $actions = [];
